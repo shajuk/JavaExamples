cec2fbb59c88ecd4133c829267a8a622a0b0e948
shajuk <shaju.shajuk@gmail.com>
2017-05-26 17:02:55 +0530

    Inital commit

---
+++ src/com/examples/immutable/demo/Employee.java
@@ -0,0 +1,12 @@
+package com.examples.immutable.demo;
+
+import java.util.Date;
+
+public final class Employee {
+	private String name;
+	private Date dateOfBirth;
+	
+	private Employee() {
+		
+	}
+}
