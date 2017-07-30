4353b044256da9c44ee27ad4eaf3e237f9bbb26c
shajuk <shajuk@users.noreply.github.com>
2017-07-30 13:46:23 +0530

    Update Main.java

--- src/examples/inheritance/covariant/method/overriding/Main.java
+++ src/examples/inheritance/covariant/method/overriding/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		EmployeeDetails p=new EmployeeDetails();
 		p.setEmployee(new Employee("Shaju", 100));
+		
 		System.out.println(p.getDetails().getName());
 		System.out.println(p.getDetails().getEmpId());
 	}
