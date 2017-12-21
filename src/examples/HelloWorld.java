02530b0f721ecae569f4b76dccf3b164a3836140
shajuk <shajuk@users.noreply.github.com>
2017-12-21 12:11:00 +0530

    chery pick

--- src/examples/HelloWorld.java
+++ src/examples/HelloWorld.java
@@ -2,7 +2,11 @@ package examples;
 
 public class HelloWorld {
 	public static void main(String[] args) {
+<<<<<<< master
 		//display greeting message 
+=======
+		//display greeting text
+>>>>>>> 9071e57 Update HelloWorld.java
 		Print.display();
 	}
 
