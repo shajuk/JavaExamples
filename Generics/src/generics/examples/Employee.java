b2a58b46f182769317d368e80584de5e651b68cf
shajuk <shaju.shajuk@gmail.com>
2018-01-06 11:16:16 +0530

    local test branch commit

--- Generics/src/generics/examples/Employee.java
+++ Generics/src/generics/examples/Employee.java
@@ -2,6 +2,7 @@ package generics.examples;
 
 public class Employee extends Person{
 	private String empid;
+	
 	public Employee(String name, Integer age, String empid) {
 		super(name, age,Employee.class.getName());
 		this.empid = empid;
